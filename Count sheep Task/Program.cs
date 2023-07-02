47b92fe38f063f18b0a33ee529d99e5137e28692
Vladyslav <vladmelhnik@gmail.com>
2023-07-02 22:11:46 +0300

    Add project files.

---
+++ Count sheep Task/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SheepTask
+{
+    internal class Program
+    {
+        public static string CountSheep(int n)
+        {
+            string result = " ";
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number of sheeps");
+    
+        }
+    }
+}
